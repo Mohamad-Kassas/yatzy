689b1ee599c2ce4e17b86f786d65ce28c29ba3d6
Mohamad Kassas <mr.mohamadkassas@gmail.com>
2024-06-07 23:48:09 -0400

    Implemented reactive design and joined the gameboard and scoreboard

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import Gameboard from './gameboard';
-import styles from '../styles/game.module.css';
+import React from "react"
+import Gameboard from "./gameboard"
+import Scoreboard from "./score_board"
+import styles from "../styles/game.module.css"
 
 function Game() {
-    return (
-        <div className={styles.gameContainer}>
-            <Gameboard />
+  return (
+    <div className={styles.backgroundContainer}>
+      <div className={styles.gameContainer}>
+        <div className={styles.gameboardContainer}>
+          <Gameboard />
         </div>
-    );
-    }
 
-export default Game;
+        <div className={styles.scoreboardContainer}>
+          <Scoreboard dices={[0, 0, 0, 0, 0]} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Game
