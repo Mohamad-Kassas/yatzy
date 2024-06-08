750edabc1bceb2397542cbd87d2aa30b17a3a803
Mohamad Kassas <mr.mohamadkassas@gmail.com>
2024-06-08 00:24:38 -0400

    Moved Game Engine logic to the highest component in the stack

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,14 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import Gameboard from "./gameboard"
 import Scoreboard from "./score_board"
 import styles from "../styles/game.module.css"
 
 function Game() {
+  const [dices, setDices] = useState([0, 0, 0, 0, 0])
+  const [keep, setKeep] = useState([false, false, false, false, false])
+  const [rollsLeft, setRollsLeft] = useState(3)
+  const [welcomeHasShown, setWelcomeHasShown] = useState(false)
+
+  const roll = () => {
+    if (rollsLeft === 0) return
+    const newDices = dices.map((dice, index) => {
+      if (keep[index]) {
+        return dice
+      } else {
+        return Math.floor(Math.random() * 6) + 1
+      }
+    })
+    setRollsLeft(rollsLeft - 1)
+    setDices(newDices)
+  }
+
+  const toggleKeep = (index) => {
+    const newKeep = [...keep]
+    newKeep[index] = !newKeep[index]
+    setKeep(newKeep)
+  }
+
+  const startButtonClick = () => {
+    roll()
+    setWelcomeHasShown(true)
+  }
   return (
     <div className={styles.backgroundContainer}>
       <div className={styles.gameContainer}>
         <div className={styles.gameboardContainer}>
-          <Gameboard />
+          <Gameboard
+            dices={dices}
+            keep={keep}
+            rollsLeft={rollsLeft}
+            welcomeHasShown={welcomeHasShown}
+            roll={roll}
+            toggleKeep={toggleKeep}
+            startButtonClick={startButtonClick}
+          />
         </div>
 
         <div className={styles.scoreboardContainer}>
